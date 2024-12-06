fb0b705a6b029225dd989ba72b8febd76bcc9604
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-12-06 20:49:01 +0530

    Update code.py

--- Question 4/code.py
+++ Question 4/code.py
@@ -1,1 +1,19 @@
+#Question: Program to find the largest of three numbers.
 
+
+num1 = int(input("Enter Your numbers: "))
+num2 = int(input("Enter Your numbers: "))
+num3 = int(input("Enter Your numbers: "))
+
+large = 0
+
+if num1 > large:
+  large = num1
+
+if num2 > large:
+  large = num2
+
+if num3 > large:
+  large = num3
+
+print("Largest is:", large)
