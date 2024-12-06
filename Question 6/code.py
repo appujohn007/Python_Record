63fbd84e8a5b7aea265d17cef2da4e810050c960
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-12-06 21:16:54 +0530

    Create code.py

---
+++ Question 6/code.py
@@ -0,0 +1,7 @@
+# Question: Program to construct patterns of stars (*), using a nested for loop.  HINT: Nested loop is a programming concept where a loop is placed inside a loop
+
+n = int(input("Enter Number of rows to be shown: "))
+
+for i in range(1, n+1):
+  for j in range(1, i+1):
+    print("*", end= "")
