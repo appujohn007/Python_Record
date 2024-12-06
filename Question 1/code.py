c7b63b30ee659fff835ba19b980d945ce6d779e2
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-12-06 20:17:02 +0530

    Update code.py

--- Question 1/code.py
+++ Question 1/code.py
@@ -1,1 +1,27 @@
+num1 = int(input("Enter Your 1st number to calculate: "))
+num1 = int(input("Enter Your 2st number to calculate: "))
+opr = input("Enter Your mathematical operator for calculation. \n eg (+, -, *, /): ")
 
+if opr == "+":
+  result = num1 + num2
+
+elif opr == "-":
+  result = num1 - num2
+
+elif opr == "*":
+  result = num1 * num2
+
+elif opr == "/":
+  if num2 != 0:
+    result = num1 / num2
+  else print("Division by zero not permitted...!")
+
+elif opr == "%":
+  if num2 != 0:
+    result = num1 % num2
+  else print("Division by zero not permitted...!")
+
+else:
+  print("Invalid operator passed...!")
+
+print(f"result: {result}")
