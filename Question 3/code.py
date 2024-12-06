69e7446bb28fd1549704aa44bcb9719f7ea3202f
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-12-06 20:43:19 +0530

    Update code.py

--- Question 3/code.py
+++ Question 3/code.py
@@ -3,15 +3,15 @@
 
 import numpy as np
 
+#makes a list 
 list = np.array([1, 2, 3, 4, 5])
-
 print("Original list: {list}")
 
-
+#add number 6 to the list
 list2 = np.append(list, 6)
-
 print("appended list:", list2)
 
+# remove 4th indexed value 
 list3 = np.delete(list, 3)
 print("deleted list:", list3)
 
