5bf049a32de6506a03261dfe131da79a6aa06df5
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-12-06 20:37:30 +0530

    Update code.py

--- Question 3/code.py
+++ Question 3/code.py
@@ -1,1 +1,13 @@
+# Question: Write a program to create, append, and remove lists in Python using NumPy.
 
+
+import numpy as np
+
+list = np.array([1, 2, 3, 4, 5])
+
+print("Original list: {list}")
+
+
+list2 = np.append(list, 6)
+
+print("appended list:", list2)
