55b37a39f27f43f84b0e15520b589c490c7f6016
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-12-06 20:27:50 +0530

    Update code.py

--- Question 2/code.py
+++ Question 2/code.py
@@ -1,1 +1,12 @@
+# Question: Create, concatenate, and print a string and access a sub-string from a given string.
+
+string1 = "Hello"
+string2 = "Everybody!"
+
+conc_str = string1 + " " + string2
+
+print(conc_str)
+
+sub_str = conc_str[6:]
+print(sub_str)
 
