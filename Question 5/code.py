8bea4aeb682b9e6c105233136ffc53076b4233f3
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-12-06 21:01:12 +0530

    Update code.py

--- Question 5/code.py
+++ Question 5/code.py
@@ -1,1 +1,8 @@
+# Question: Convert temperature values back and forth between Celsius (c), and Fahrenheit (f). [Formula: c/5= f-32/9] 
 
+mode = input("Enter "C" to convert from Fahrenheit to Celsius, "F" for Celsius to Fahrenheit: ")
+
+if mode in [c, C]:
+  fahrenheit = int(input("Enter your temperature value in Fahrenheit. eg: 45"))
+  value = 5/9 * (fahrenheit - 32)
+  print("{fahrenheit} Fahrenheit is {value}° Celsius")
