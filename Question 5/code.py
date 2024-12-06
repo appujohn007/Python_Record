16c4f974e42a7072778f7c40d8f7013a9c1539ec
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-12-06 21:04:55 +0530

    Update code.py

--- Question 5/code.py
+++ Question 5/code.py
@@ -3,6 +3,6 @@
 mode = input('Enter "C" to convert from Fahrenheit to Celsius, "F" for Celsius to Fahrenheit: ')
 
 if mode in ["c", "C"]:
-  fahrenheit = int(input("Enter your temperature value in Fahrenheit. eg: 45"))
+  fahrenheit = int(input("Enter your temperature value in Fahrenheit. eg: 45: "))
   value = 5/9 * (fahrenheit - 32)
-  print("{fahrenheit} Fahrenheit is {value}° Celsius")
+  print(f"{fahrenheit} Fahrenheit is {value}° Celsius")
