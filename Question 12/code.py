debdd326f221aa0d3b0f7c2106e7b6af015356e7
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-12-07 13:31:52 +0530

    Create code.py

---
+++ Question 12/code.py
@@ -0,0 +1,6 @@
+#Question
+"""
+A program that accepts the lengths of three sides of a triangle as inputs. 
+The program should output whether or not the triangle is a right triangle (Recall from the Pythagorean Theorem that in a right triangle, the square of one side equals the sum of the squares of the other two sides). 
+Implement using functions
+"""
