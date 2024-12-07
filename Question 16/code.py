c8520653ef311b15a38d7fc6e62412383e140e86
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-12-07 13:27:16 +0530

    Create code.py

---
+++ Question 16/code.py
@@ -0,0 +1,15 @@
+#Question: 
+"""
+Write a program to play a sticks game in which there are 16 sticks. 
+Two players take turns to play the game. Each player picks one set of sticks (needn’t be adjacent) during his turn.
+A set contains 1, 2, or 3 sticks. The player who takes the last stick is the loser. The number of sticks in the set is to be input.
+"""
+
+
+
+def main():
+  print("Welcome to the STICK GAME")
+  player = 1
+  
+
+main()
